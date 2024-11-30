4fd75be9263dded307cfab298872bdbe94264876
Jamon27 <moiseev27@gmail.com>
2024-11-30 12:49:07 +0200

    Init react app

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser'; // Import the TypeScript parser
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+
+    // Language options
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser, // Pass the actual parser object
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    // Plugins
+    plugins: {
+      prettier: pluginPrettier,
+      react: pluginReact,
+      '@typescript-eslint': tseslint,
+    },
+
+    // Rules
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'prettier/prettier': 'error', // Enable Prettier as an ESLint rule
+    },
+
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
+
+  // Prettier configuration
+  prettierConfig,
+];
